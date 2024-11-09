fb868f4952d761f6ba2b44247b985b37c4243567
Andrew <andrewchau1993@gmail.com>
2024-11-08 20:25:53 -0800

    introduction of fragment to clean dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -69,7 +69,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
