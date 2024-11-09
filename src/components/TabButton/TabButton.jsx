09468bcb207408eb82a44c13e607c9c4ddb23a46
Andrew <andrewchau1993@gmail.com>
2024-11-08 21:16:30 -0800

    you could also rename it to ...props but its not as readable

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -2,7 +2,7 @@ export default function TabButton({ label,isSelected, ...props }) {
   // console.log('TabButton rendered');
   return (
   <li>
-    <button className={isSelected ? 'active' : undefined} {...TabButton}>{label}</button>
+    <button className={isSelected ? 'active' : undefined} {...props}>{label}</button>
   </li>
   );
 }
